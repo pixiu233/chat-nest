51eb9ef3a4ef0f63ca127ba86e5ff8bac98aed8a
songsikai <526851305@qq.com>
2024-05-11 17:13:48 +0800

    crud

---
+++ code.md
@@ -0,0 +1,4 @@
+#快速crud
+$ nest g resource users
+// 像这样传递`--no-spec`参数`nest g resource users --no-spec`来避免生成测试文件
+$ nest g resource users --no-spec
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './logical/user/user.service';
+import { UserController } from './logical/user/user.controller';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodosModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
